c78631146e04f1c2fa5c5bd43224e7342d44bde9
treeswithlove <monet.2017@gmail.com>
2019-05-14 15:03:34 -0400

    shows server send

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const routes = require('./routes/index.js')
 app.use(logger('dev'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(`${__dirname}/client/build`))
+// app.use(express.static(`${__dirname}/client/build`))
 
 // app.use('/', routes)
 
-app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
-})
+// app.get('/*', (req, res) => {
+//     res.sendFile(`${__dirname}/client/build/index.html`)
+// })
 app.get('/', (req, res) => {
     res.send("Hello World")
   })
