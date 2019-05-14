e918b4745d0f60ddc8ffbe52ab59b5b818c8681e
treeswithlove <monet.2017@gmail.com>
2019-05-14 13:31:31 -0400

    basic setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const logger = require('morgan')
+const app = express()
+const routes = require('./routes/index.js')
+
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(`${__dirname}/client/build`))
+
+app.use('/', routes)
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
+app.get('/', (req, res) => {
+    res.send("Hello World")
+  })
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log('App is up and running on port ' + PORT)
+})
